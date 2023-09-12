3c3f7899afc53aa88f7465a0f3f5c7e0ef43a85d
Ayush Bodade <ayushbodade.official@gmail.com>
2023-09-13 02:31:17 +0530

    Removed api key

--- app1.py
+++ app1.py
@@ -14,7 +14,7 @@ from langchain.agents.agent_toolkits import (
 app = Flask(__name__)
 
 # Set API key for OpenAI Service
-os.environ['OPENAI_API_KEY'] = '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'
+os.environ['OPENAI_API_KEY'] = 'your_openai_api_key'
 
 # Create an instance of OpenAI LLM
 llm = OpenAI(temperature=0.1, verbose=True)
